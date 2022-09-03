ecacb1d03c2f94b1f5626e6e6723afaf8cd5fe65
prashant3jan <prashant3jan@gmail.com>
2022-09-03 15:36:57 +0530

    updated Visit entity to use JPA, closes #41

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -2,6 +2,9 @@ package guru.springframework.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name="pets")
 public class Pet extends BaseEntity {
@@ -10,34 +13,28 @@ public class Pet extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "type_id")
     private PetType petType;
-
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
-
     @Column(name = "birth_date")
     private LocalDate birthDate;
-
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public PetType getPetType() {
         return petType;
     }
-
     public void setPetType(PetType petType) {
         this.petType = petType;
     }
-
     public Owner getOwner() {
         return owner;
     }
-
     public void setOwner(Owner owner) {
         this.owner = owner;
     }
@@ -48,5 +45,11 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
