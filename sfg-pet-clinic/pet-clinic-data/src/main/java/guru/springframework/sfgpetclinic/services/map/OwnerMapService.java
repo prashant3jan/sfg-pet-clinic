67e54531ef824651b29deb58ba2702b4e4141065
prashant3jan <prashant3jan@gmail.com>
2022-09-26 18:43:45 +0530

    added CRUD test for OwnerMapService, closes #52

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerMapService.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerMapService.java
@@ -65,11 +65,15 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public void deleteById(Long id) {
-        this.deleteById(id);
+        super.deleteById(id);
     }
 
     @Override
     public Owner findByLastName(String lastName) {
-        return null;
+        return this.findAll()
+                .stream()
+                .filter(owner -> owner.getLastName().equalsIgnoreCase(lastName))
+                .findFirst()
+                .orElse(null);
     }
 }
