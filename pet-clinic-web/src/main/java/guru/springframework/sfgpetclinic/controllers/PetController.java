78318f5efdd93179df8e7298dd5eeaad92f8e5a7
Prashant Singh <prashant3jan@gmail.com>
2023-04-28 10:46:35 +0530

    added ability to add or update pet, #58

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/PetController.java
@@ -1,18 +1,24 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import guru.springframework.sfgpetclinic.services.PetService;
 import guru.springframework.sfgpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/owners/{ownerId}")
@@ -29,6 +35,7 @@ public class PetController {
         this.petTypeService = petTypeService;
     }
 
+
     @ModelAttribute("types")
     public Collection<PetType> populatePetTypes(){
         return petTypeService.findAll();
@@ -42,5 +49,62 @@ public class PetController {
     @InitBinder("owner")
     public void initOwnerBinder(WebDataBinder dataBinder){
         dataBinder.setDisallowedFields("id");
+    }
+
+    @GetMapping("/pets/new")
+    public String initCreationForm(Owner owner, Model model) {
+        PetType petType = PetType.builder().name("").build();
+
+        Pet pet = Pet.builder().name(null).petType(petType).owner(owner).birthDate(null).visits(null).build();
+        if(owner.getPets() == null){
+           Set<Pet> pets =  new HashSet<>();
+           owner.setPets(pets);
+        }
+        owner.getPets().add(pet);
+        model.addAttribute("pet", pet);
+        return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
+    }
+
+    @PostMapping("/pets/new")
+    public String processCreationForm(Owner owner, @Valid Pet pet, BindingResult result, ModelMap model){
+        if (StringUtils.hasLength(pet.getName()) && pet.isNew() && owner.getPet(pet.getName(),true) != null){
+            result.rejectValue("name", "duplicate","already exists");
+        }
+        if(owner.getPets() == null){
+            Set<Pet> pets = new HashSet<>();
+            owner.setPets(pets);
+        }
+
+        owner.getPets().add(pet);
+        if(result.hasErrors()){
+            model.put("pet",pet);
+            return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
+        }else{
+            petService.save(pet);
+            return "redirect:/owners/"+owner.getId();
+        }
+    }
+
+    @GetMapping("/pets/{petId}/edit")
+    public String initUpdateForm(@PathVariable Long petId, Model model){
+        model.addAttribute("pet",petService.findById(petId));
+        return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
+    }
+
+    @PostMapping("/pets/{petId}/edit")
+    public String processUpdateForm(@Valid Pet pet, BindingResult result, Owner owner, Model model){
+        if(result.hasErrors()){
+            pet.setOwner(owner);
+            model.addAttribute("pet",pet);
+            return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
+        }else{
+            if (owner.getPets() == null){
+                Set<Pet> pets = new HashSet<>();
+                owner.setPets(pets);
+            }
+            owner.getPets().add(pet);
+            petService.save(pet);
+            return "redirect:/owners/"+owner.getId();
+        }
     }
 }
